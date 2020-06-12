4319a7d6e97b018e7b2e92d008e4b1b3c94b2adf
Mourad <mourad.eyes@gmail.com>
2020-06-12 19:33:48 +0100

    chore: fix image url

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     defaultTitle: `izmjs`,
     siteTitleShort: `izmjs`,
     siteDescription: `a Highly maintainable & modular NodeJS project architecture`,
-    siteUrl: `https://izmjs.netlify.app`,
+    siteUrl: `https://raw.githubusercontent.com/izmjs/izmjs.github.io/master/static/`,
     siteAuthor: "izmjs",
     siteImage: `/banner.png`,
     siteLanguage: `en`,
